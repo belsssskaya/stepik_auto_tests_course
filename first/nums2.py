9106c95bf94b87fbfc08043131d687a16765a81e
belsssskaya <kate39825@gmail.com>
2023-12-20 00:16:27 +0700

    копия nums

---
+++ first/nums2.py
@@ -0,0 +1,16 @@
+def Sum(a, b):
+    return a + b
+
+
+def Razn(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print(Sum(5, 6))
+    print(Razn(3, 2))
+    print(mul(3, 5))
