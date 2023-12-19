a381273bcbf9cb9940402d540349e0b24513fd61
belsssskaya <kate39825@gmail.com>
2023-12-19 23:03:53 +0700

    добавлен модуль strings и функция mul, text удален

--- first/nums.py
+++ first/nums.py
@@ -1,12 +1,16 @@
-def Sum(a,b):
-    return a+b
+def Sum(a, b):
+    return a + b
 
-text='privet'
 
-def Razn (a, b):
-    return a-b
+def Razn(a, b):
+    return a - b
 
-if __name__=="__main__":
-    print (Sum(5,6))
-    print (Razn(3,2))
 
+def mul(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print(Sum(5, 6))
+    print(Razn(3, 2))
+    print(mul(3 , 4))
